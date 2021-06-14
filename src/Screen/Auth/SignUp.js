e851fa410910119ccb7c8791ac869e59f04f1b08
@muniasamyrama <muniasamy.ramakrishnan@siamcomputing.com>
2021-06-14 13:41:59 +0530

    updated

--- src/Screen/Auth/SignUp.js
+++ src/Screen/Auth/SignUp.js
@@ -11,6 +11,7 @@ var { height, width } = Dimensions.get('window')
 const SignUpScreen = (props) => {
   const [userName, setUserName] = useState('');
   const [email,setEmail] = useState('');
+  const [emailError,setEmailError] = useState(true);
   const [pwd,setPwd] = useState('');
   const [hidePassword,setHidePassword]=useState(false);
   const _onDone = () => {
@@ -20,14 +21,14 @@ const SignUpScreen = (props) => {
  }
       return   <View style={{flex:1,backgroundColor:'#E5E5E5'}}>
        <KeyboardAwareScrollView scrollEnabled={true} keyboardShouldPersistTaps="handled" contentContainerStyle={{ flexGrow:1 }} keyboardDismissMode="interactive">
-       <View style={{flex:0.05}}/>
+       <View style={{flex:0.1}}/>
       <View style={{flex:0.1}}>
       <Text style={{fontFamily:FONTS.PROXIMA_NOVA,fontWeight:'bold',fontSize:18,marginTop:10,marginLeft:20}}>Sign Up</Text> 
       <Text style={{fontFamily:FONTS.PROXIMA_NOVA,fontSize:18,marginTop:5,marginLeft:20,opacity:0.2}}>to challenge your friends and win loot!
 </Text>
       </View>
-      
-      <View style={{flex:0.4,backgroundColor:'#FFFFFF',marginLeft:15,marginRight:15,borderRadius:20}}>
+      <View style={{flex:0.05}}/>
+      <View style={{flex:0.1,backgroundColor:'#FFFFFF',marginLeft:15,marginRight:15,borderRadius:20}}>
       <View style={[styles.passwordContainer,{marginTop:40}]}>
       <TextInput
       label="Username"
@@ -44,17 +45,28 @@ const SignUpScreen = (props) => {
     <View style={styles.passwordContainer}>
         <TextInput
       label="Email"
+
       mode="outlined"
       value={email}
       style={styles.inputStyle}
-      outlineColor='#757575'
+      placeholderTextColor={emailError===true?'red':null}
+      outlineColor={emailError===true?'red':'#757575'}
       onChangeText={email => setEmail(email.trim())}
       // style={{margin:20}}
     //   value={text}
     
     />
+
+
     </View>
+    {emailError===true?
+    <View style={{marginLeft:20,marginTop:-10}}>
+    <Text styles={{fontSize:8,fontFamily:FONTS.PROXIMA_NOVA,color:'red',fontWeight:'100'}}>Email already exists</Text>
+    </View>:null}
+    
+
     <View style={styles.passwordContainer}>
+   
         <TextInput
       label="Password"
       mode="outlined"
@@ -62,27 +74,29 @@ const SignUpScreen = (props) => {
       style={styles.inputStyle}
       secureTextEntry={hidePassword}
       outlineColor='#757575'
+      right={<TextInput.Icon name="eye" color="#CCCCCC"/>}
       onChangeText={pwd => setPwd(pwd.trim())}
       // style={{margin:20}}
     //   value={text}
     
   
     />
-    <TouchableOpacity   style={{position:'absolute',right:10}} onPress={() =>alert('hi')}>
+    {/* <TouchableOpacity   style={{position:'absolute',right:10}} onPress={() =>alert('hi')}>
 
 <Icon
     name="eye"
     onPress={() =>alert('hi')}
     color={hidePassword?'#0087ED':'#E5E5E5'}
     size={22}
-/></TouchableOpacity>
+/></TouchableOpacity> */}
     </View>
-    
+    <View style={{marginLeft:20,flex:0.1,opacity:0.4,justifyContent:'flex-end',marginBottom:10}}>
     <Checkbox
-
-
-  checked={true}
+    contentContainerStyle={{borderWidth:0.1,borderColor:'#757575',opacity:0.6}}
+status={'unchecked '}
 />
+<Text style={{position:'absolute',left:30,bottom:8}}>I certify that I am 13 years or older.</Text>
+</View>
       </View>
       <View style={{flex:0.1}}></View>
       <View style={{flex:0.03,alignSelf:'center',marginLeft:width/3}}>
@@ -101,7 +115,7 @@ const SignUpScreen = (props) => {
   
                <Text style={{textAlign:'center',color:'#0087ED',fontWeight:'bold',marginLeft:width/9}}onPress={()=>props.navigation.navigate('ForgotPasswordScreen')}>Login</Text>
       </View>
-
+      <Text style={{marginLeft:20,textAlign:'center',opacity:0.4,fontFamily:FONTS.PROXIMA_NOVA,fontSize:12}}>by clicking “Signup” button you agree to T&C.</Text>
 </KeyboardAwareScrollView>
 </View> ;
 };
